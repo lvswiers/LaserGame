30c51fe6013907aff636016fa2ac5f46c8a37b65
Leandra Swiers <l.swiers@eaglescience.nl>
2020-06-03 15:12:09 +0200

    Make laserbox shoot bullet and collide with mirror

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    private float actualSpeed;
+
+    private void Start() {
+        actualSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position += transform.right * Time.deltaTime * actualSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        actualSpeed = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        actualSpeed = 0;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        actualSpeed = 0;
+    }
+
+}
