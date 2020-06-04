a423fc22bda2c432ba2fe6faf4d20e4752fc7e5f
Leandra Swiers <l.swiers@eaglescience.nl>
2020-06-04 17:02:29 +0200

    Finish dummy level, with automatic start and end

---
+++ Assets/Scripts/LaserTarget.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserTarget : MonoBehaviour {
+
+    private string tag = "Bullet";
+    
+    private bool destroyBullet(Collision collision) {
+        GameObject collisionObject = collision.gameObject;
+
+        if (collisionObject.tag == tag) {
+            Destroy(collisionObject);
+            return true;
+        } else {
+            return false;
+        }
+       
+    }
+
+    private void changeColour() {
+        var rend = GetComponent<Renderer>();
+        Material material = rend.material;
+        material.SetColor("_Color",new Color(0.2f, 0.5f, 0));
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision) {
+        bool isDestroyed = destroyBullet(collision);
+
+        if (isDestroyed){
+            changeColour();
+        }
+    }
+}
---
+++ Assets/Scripts/StartLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel : MonoBehaviour {
+
+    public Grid Grid;
+    public float xRotation;
+    public float yRotation;
+    public float zRotation;
+
+    void Start() {
+        Grid.gameObject.transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}
