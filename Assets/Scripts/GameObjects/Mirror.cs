18ae13d22f9424f0baee711e3a60205917f2c79e
unknown <l.v.swiers@gmail.com>
2020-08-25 14:04:17 +0200

    Add a normal vector to all mirror types

--- Assets/Scripts/GameObjects/Mirror.cs
+++ Assets/Scripts/GameObjects/Mirror.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
-using System.Windows.Media.Media3D;
 
 public class Mirror : MonoBehaviour
 {
-    private Vector3D normal;
-
-    public Vector3D Normal { get => normal; }
+    public Vector3 normal;
 }
