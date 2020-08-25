750f5a7a8a79bc0163d40658c372d4a72568bd5c
unknown <l.v.swiers@gmail.com>
2020-08-25 12:06:37 +0200

    Different Unity version etc

--- Assets/Scripts/Editor/PrefabBrush.cs
+++ Assets/Scripts/Editor/PrefabBrush.cs
@@ -55,7 +55,7 @@ namespace UnityEditor {
     }
 
     [CustomEditor(typeof(PrefabBrush))]
-    public class PrefabBrushEditor : GridBrushEditorBase {
+    public class PrefabBrushEditor : UnityEditor.Tilemaps.GridBrushEditorBase {
         private PrefabBrush prefabBrush { get { return target as PrefabBrush; } }
 
         private SerializedProperty m_Prefabs;
