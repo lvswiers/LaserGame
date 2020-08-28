38eb2a20d04c5c66dcc6cd0156f5bb65f8eabbb4
unknown <l.v.swiers@gmail.com>
2020-08-28 16:03:53 +0200

    Make sure no errors are generated when bullet collides with random things

--- Assets/Scripts/GameObjects/Projectile.cs
+++ Assets/Scripts/GameObjects/Projectile.cs
@@ -17,13 +17,15 @@ namespace GameObjects {
         }
 
         private void OnCollisionEnter(Collision collision) {
-    
+            
             Mirror mirror = collision.gameObject.GetComponent<Mirror>();
-            Vector3 normal = mirror.Normal;
+            if (mirror) {
+                Vector3 normal = mirror.Normal;
 
             // Calculate the new velocity by using the normal vector of the mirror and the original velocity, using projection/dot product
             // Example of a derivation: https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
             velocity = velocity - 2 * (Vector3.Dot(velocity, normal)* normal);
+            }  
         }
 
         public void StartMoving() {
